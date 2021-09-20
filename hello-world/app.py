9c6838a9a73cc38f7511abb149ffb661e5927fa2
Paul Yuknewicz <paulyuk@microsoft.com>
2021-09-20 09:43:25 -0700

    Added tye run

--- hello-world/app.py
+++ hello-world/app.py
@@ -10,6 +10,8 @@ import time
 dapr_port = os.getenv("DAPR_HTTP_PORT", 3500)
 dapr_url = "http://localhost:{}/v1.0/invoke/nodeapp/method/neworder".format(dapr_port)
 
+print("Started.  Target URL is: {}".format(dapr_url))
+
 n = 0
 while True:
     n += 1
